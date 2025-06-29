369f8b576bb90a198563309824fbf67f553d3429
Elana Van Impe Delmote <elana.van.impe.delmote@student.howest.be>
2025-06-29 22:36:17 +0200

    eroor

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import db from '@astrojs/db';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [db()]
+  integrations: [db()],
   site: 'https://elanaguinevere.github.io',
   base: '/my-portfolio',
 });

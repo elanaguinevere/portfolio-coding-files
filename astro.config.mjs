0abadb730e5ab9731698e6267e48d708803d4de2
Elana Van Impe Delmote <elana.van.impe.delmote@student.howest.be>
2025-06-30 15:37:04 +0200

    astroconfig update

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,12 @@ import { defineConfig } from 'astro/config';
 
 import db from '@astrojs/db';
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [db()],
   site: 'https://elanaguinevere.github.io',
   base: '/my-portfolio',
+  adapter: netlify(),
 });
